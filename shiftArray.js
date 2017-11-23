506cbac28c8fff01a66d63526ee7f664e03ed3ca
cam <cam.marshall940@yahoo.com>
2017-11-22 23:36:26 -0700

    Added

--- shiftArray.js
+++ shiftArray.js
@@ -1,7 +1,3 @@
-//Given an array x (e.g. [1,5, 10, 7, -2]), create an algorithm that
-//shifts each number by one (to the front).  For example when the program 
-//is done x (assuming it was [1,5,10,7,-2]) should become [5,10,7,-2, 0].
-
 function shift(num, arr){
 	var length = arr.length;
 	var idx;
